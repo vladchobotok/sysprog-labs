4d5855b801ee64f698ef50e66d8ae06c61d7dbe8
vladchobotok <vladchobotok@gmail.com>
2022-10-01 20:49:35 +0300

    Added Lab1
    Added Lab1

---
+++ Lab1/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+9
+	words.txt,b\5\b50c83fae7efacc180a53aea3fdd96a39dc2cfe2
---
+++ Lab1/src/com/knu/WordsSorter.java
@@ -0,0 +1,29 @@
+package com.knu;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class WordsSorter {
+    public static void main(String[] args) {
+        File file = new File("words.txt");
+        TreeSet<String> setOfWords = new TreeSet<>();
+        WordsSorter.readTheWords(file, setOfWords);
+        System.out.println(String.join(", ", setOfWords));
+    }
+
+    public static void readTheWords(File file, SortedSet<String> treeSet){
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String[] words = scanner.nextLine().split("[\\W\\d]");
+                for (String word: words) {
+                    if(!word.equals("")){
+                        treeSet.add(word.substring(0, Math.min(word.length(), 30)));
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+    }
+}
